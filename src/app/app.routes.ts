0b2cd28e01e07c8c8857a020cda22492c93007aa
GimhaniRuberu <gayathrigimhani88@gmail.com>
2025-09-21 11:46:51 +0530

    feat: add admin route to load AdminModule for admin dashboard access

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,5 +2,6 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './basic/login/login.component';
 
 export const routes: Routes = [
-    { path: "",component:LoginComponent}
+    { path: "",component:LoginComponent},
+    { path: "admin",loadChildren: () => import('./admin/admin.module').then(m => m.AdminModule) }
 ];
