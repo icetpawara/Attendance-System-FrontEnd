768f689403e70e2037588a01512143adee901d30
GimhaniRuberu <gayathrigimhani88@gmail.com>
2025-09-21 11:43:24 +0530

    feat: enhance login functionality to redirect admins to dashboard after login

--- src/app/basic/login/login.component.ts
+++ src/app/basic/login/login.component.ts
@@ -4,6 +4,7 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { AuthService } from '../basic-services/auth.service';
 import { NzMessageService } from 'ng-zorro-antd/message';
 import { UserStorageService } from '../basic-services/user-storage.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -17,7 +18,8 @@ export class LoginComponent {
 
   constructor(private fb: FormBuilder,
     private authService: AuthService,
-    private message: NzMessageService
+    private message: NzMessageService,
+    private router: Router
   ){}
 
 
@@ -30,6 +32,10 @@ export class LoginComponent {
   submitForm(){
     this.authService.loginUser(this.loginForm.value).subscribe(res=>{
       UserStorageService.saveUser(res);
+
+      if(UserStorageService.isAdminLoggedIn()){
+        this.router.navigateByUrl('/admin/dashboard');
+      }
         console.log(res);
     },error=>{
       this.message
