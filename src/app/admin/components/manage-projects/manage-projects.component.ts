61b345c097a2f4ec411a5792124d5af1fbdfd3ec
GimhaniRuberu <gayathrigimhani88@gmail.com>
2025-09-21 12:45:00 +0530

    feat: initialize project form in ManageProjectsComponent with validation

--- src/app/admin/components/manage-projects/manage-projects.component.ts
+++ src/app/admin/components/manage-projects/manage-projects.component.ts
@@ -1,11 +1,26 @@
 import { Component } from '@angular/core';
+import { SharedModule } from '../../../shared/shared.module';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-manage-projects',
-  imports: [],
+  imports: [SharedModule],
   templateUrl: './manage-projects.component.html',
   styleUrl: './manage-projects.component.scss'
 })
 export class ManageProjectsComponent {
+
+  projectForm!:FormGroup;
+
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit(){
+    this.projectForm = this.fb.group({
+      name: [null,[Validators.required]],
+      duration: [null,[Validators.required]],
+      startDate: [null,[Validators.required]],
+
+    });
+}
 
 }
