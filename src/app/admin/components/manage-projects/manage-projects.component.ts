3e825546900bbd9990078a327f1b1b304b7150ce
GimhaniRuberu <gayathrigimhani88@gmail.com>
2025-09-21 12:05:57 +0530

    feat: create ManageProjectsComponent with HTML, SCSS, and unit tests

---
+++ src/app/admin/components/manage-projects/manage-projects.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-manage-projects',
+  imports: [],
+  templateUrl: './manage-projects.component.html',
+  styleUrl: './manage-projects.component.scss'
+})
+export class ManageProjectsComponent {
+
+}
