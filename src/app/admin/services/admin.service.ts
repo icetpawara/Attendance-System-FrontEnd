c55d13b6b79a8fa67b7773b9edbc9ecee2d29082
pawara-mmns <pawarasamarawickrama@gmail.com>
2025-09-28 10:08:57 +0530

    feat: implement AdminService for project management with addProject method

---
+++ src/app/admin/services/admin.service.ts
@@ -0,0 +1,17 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+const url = "http://localhost:8080";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AdminService {
+
+  constructor(private http: HttpClient) { }
+
+addProject(project: any) : Observable<any> {
+  return this.http.post(url + "/api/projects/add", project);
+  }
+}
